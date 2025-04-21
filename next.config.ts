efb945d6935f78ab31d9316839e4bc745c8d486c
gyugod93 <gyu4016@naver.com>
2025-04-21 10:43:50 +0900

    lighthouse accessibility의 render-blocking리소스 제거 위해 @next/bundle-analyzer + critical CSS 추가하여 90점 이상 달성

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+import NextBundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    optimizeCss: true,
+  },
+  webpack(config) {
+    return config;
+  },
 };
 
-export default nextConfig;
+export default NextBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true", // 번들 분석 활성화
+})(nextConfig);
